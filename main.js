b0ecc3451f5b022e0c39f2b8c8a716f423940b04
JassonSilva <jasons_dc13@yahoo.com>
2024-12-02 00:16:33 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,48 @@
+var viewCartButton = document.getElementsByClassName('view-cart')[0];
+var shoppingCart = document.getElementsByClassName('shopping-cart')[0];
+var products = document.getElementsByClassName('products')[0];
+
+var cart = [];
+
+viewCartButton.addEventListener('click', function () {
+	if (shoppingCart.classList.contains('show')) {
+		shoppingCart.className = 'shopping-cart';
+	} else {
+		shoppingCart.className += ' show';
+	}
+});
+
+products.addEventListener('click', function (e) {
+	if (e.target.classList.contains('add-to-cart')) {
+
+		var itemName = e.target.closest('.item').getAttribute('data-name');
+    var itemPrice = e.target.closest('.item').getAttribute('data-price');
+
+    var product = {
+      name: itemName,
+      price: itemPrice
+    };
+
+    cart.push(product);
+    renderCart()
+	}
+});
+
+var renderCart = function () {
+  var cartList = document.getElementsByClassName('cart-list')[0];
+
+  while(cartList.hasChildNodes()) {
+    cartList.removeChild(cartList.firstChild);
+  }
+
+  var items = '';
+
+  for (var i = 0; i < cart.length; i++) {
+    items += '<div>' + cart[i].name
+      + ' - $' + cart[i].price + '</div>';
+      console.log(items)
+
+  }
+  
+  cartList.innerHTML = items;
+};
